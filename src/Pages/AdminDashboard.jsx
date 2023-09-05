63f694c654b8130931ebf1435424cdb7323f357f
nunjachomp <nunjachomp@gmail.com>
2023-09-05 22:27:25 +0300

    Can Now Toggle Admins

--- src/Pages/AdminDashboard.jsx
+++ src/Pages/AdminDashboard.jsx
@@ -1,28 +1,43 @@
-import axios from 'axios'
-import React from 'react'
+import axios from "axios";
+import React, {useState, useEffect} from "react";
 
 export const AdminDashboard = () => {
+  const [usersList, setUsersList] = useState([])
 
-    // const getAllUsers = async () => {
-    //     // await axios.get('http://localuser:8080/users')
-
-    // }
-    const toggleAdmin = async ()=> {
-        await axios.put("http://localhost:8080/users/toggleadmin", { id:1 }, {withCredentials: true})
+  const showAllUsers = async () => {
+    try {
+    const res = await axios.get(`${process.env.REACT_APP_SERVER_URL}/users/`, { withCredentials: true });
+    console.log(res.data)
+    setUsersList(res.data);
+    } catch (err) {
+      console.log(err);
     }
+  }
+  
+  useEffect(() => {
+    showAllUsers()
+  }, [])
 
+  const toggleAdmin = async (id) => {
+    await axios.put(`${process.env.REACT_APP_SERVER_URL}/users/toggleadmin`, {id}, { withCredentials: true });
+  };
 
   return (
     <>
-    <br/>
-    <br/>
-    <br/>
-    <br/>
-    <br/>
-    <div>AdminDashboard</div>
-    <button onClick={()=>toggleAdmin(2)}>Make Admin</button>
+      <br /><br /><br /><br /><br />
+      <div>AdminDashboard</div>
+      {/* Need to return confirmation to client */}
+      <br /><br /><br /><br />
+      <div>
+      {usersList.length > 0 ? (
+       usersList.map((user) => <div key={user.id}>{user.name} {user.email} {user.isAdmin}
+       <button onClick={() => toggleAdmin(user.id)}>Make Admin</button></div>)
+     ) : (
+       <div>Loading...</div>
+     )}
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default AdminDashboard
+export default AdminDashboard;
